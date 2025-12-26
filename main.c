4ef6db0d6b5433b093c515fe7c9bc5ec8067db7f
OliWan9 <oliver.hitz.7@gmail.com>
2025-12-26 18:14:21 +0100

    Read words.txt file

--- main.c
+++ main.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <windows.h>
 
 char* GetHangman(int stage) { // Stage: Value 0-7
     char* response;
@@ -69,9 +68,19 @@ char* GetHangman(int stage) { // Stage: Value 0-7
 }
 
 int main(void) {
-    char word[256][128]; // 256 Bytes per word, 128 words = 32768 Bytes
+    char word[128][256]; // 256 Bytes per word, 128 words = 32768 Bytes
 
+    FILE *input = fopen("../words.txt", "r");
 
+    if (input == NULL) {
+        printf("Error: Could not open words.txt\n");
+    }
+    else {
+        char rawWord[256];
+        fgets(rawWord, 256, input);
+        printf("%s", rawWord);
+    }
+    fclose(input);
 
     return 0;
 }
