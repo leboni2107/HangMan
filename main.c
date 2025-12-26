60630ae5d00a8b60ec9dbfb13481a20e0b87a909
OliWan9 <oliver.hitz.7@gmail.com>
2025-12-26 17:22:33 +0100

    GetHangman() function

--- main.c
+++ main.c
@@ -1,6 +1,70 @@
 #include <stdio.h>
 
+char* GetHangman(int stage) {
+    /*
+     * Stage: Value 0-7
+     */
+    char* response;
+    switch (stage) {
+        default:
+            response = "Invalid Stage";
+            break;
+        case 1:
+            response = "  +---+\n"
+                       "  |   |\n"
+                       "      |\n"
+                       "      |\n"
+                       "      |\n"
+                       "      |\n";
+        case 2:
+            response = "  +---+\n"
+                       "  |   |\n"
+                       "  O   |"
+                       "      |"
+                       "      |"
+                       "      |";
+        case 3:
+            response = "  +---+\n"
+                       "  |   |"
+                       "  O   |"
+                       "  |   |"
+                       "      |"
+                       "      |";
+        case 4:
+            response = "  +---+\n"
+                       "  |   |"
+                       "  O   |"
+                       " /|   |"
+                       "      |"
+                       "      |";
+        case 5:
+            response = "  +---+\n"
+                       "  |   |"
+                       "  O   |"
+                       " /|\\  |"
+                       "      |"
+                       "      |";
+        case 6:
+            response = "  +---+\n"
+                       "  |   |"
+                       "  O   |"
+                       " /|\\  |"
+                       " /    |"
+                       "      |";
+        case 7:
+            response = "  +---+\n"
+                       "  |   |"
+                       "  O   |"
+                       " /|\\  |"
+                       " / \\  |"
+                       "      |";
+
+    }
+    return response;
+}
+
 int main(void) {
-    printf("Hello, World!\n");
+    printf(GetHangman(1));
+
     return 0;
 }
